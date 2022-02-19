dc91ea86a0cb58bd01c41ac5ffbba96ba6940be1
Dylan Andres <dylanrandres@gmail.com>
2022-02-19 00:26:21 -0500

    login

--- api.js
+++ api.js
@@ -1,17 +1,99 @@
-// API function calls to interface with database
+// API function calls
 const User = require('./models/user.model.js')
+const nodemailer = require('nodemailer')
 
-async function addUser(username, password, email) {
-    await User.create({
-        username: username,
-        password: password,
-        email: email,
-        verified: false,
+async function addUser(username, password, email, res) {
+    // Check if username or email is taken
+    await User.findOne({$or: [
+        {username: username},
+        {email: email}
+    ]}).then( async (user) => {
+        // Username or email taken
+        if (user) 
+            res.send("Username or email taken")
+        // Create user and put in database
+        else {
+            await User.create({
+                username: username,
+                password: password,
+                email: email,
+                verified: false,
+            })
+            sendVerificationEmail(email)
+            res.send("registered!")
+        }
     })
 }
 
-async function verifyUser(email) {
-    await User.findOneAndUpdate({email: email}, {verified: true});
+async function verifyUser(email, key, res) {
+    // Verify key
+    if (key == "abracadabra") {
+        // Check if email exists
+        await User.findOne({email: email})
+        .then( async (user) => {
+            if (!user) {
+                res.status(404).send("Email does not exist")
+            }
+            else {
+                // Change verification status
+                await User.findOneAndUpdate({email: email}, {verified: true});
+                res.send(`
+                    ${email} verified!
+                `);
+            }
+        })
+
+    }
+    // Wrong key
+    else {
+        res.status(401).send("Wrong key")
+    }
 }
 
-module.exports = { addUser, verifyUser }
+async function login(username, password, res) {
+    await User.findOne({$and: [
+        {username: username},
+        {password: password},
+    ]}).then( (user) => {
+        if (!user)
+            res.status(401).send("Incorrect credentials")
+        else {
+            if (user.verified) {
+                res.send(`Logged in as ${username}`)
+            }
+            else {
+                res.status(403).send("This user has not been verified")
+            }
+        }
+    })
+}
+
+function sendVerificationEmail(email) {
+     // User added to database, send a verification email
+     let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465, // Secure SMTP port
+        secure: true,
+        auth: {
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASS,
+        }
+    })
+    transporter.sendMail({
+        from: '"Hot Pink" <team.hotpink.inc@gmail.com>',
+        to: email,
+        subject: "Verify Email Address",
+        html: `
+        <body>
+        Thanks for registering for an account! Before we get started, 
+        we just need to confirm that this is you. <br><br>
+        Click below to verify your email address: <br><br>
+        <a href="http://localhost:8080/verify?email=${email}&key=abracadabra">
+        Verify</a><br><br>
+        ${new Date()}
+        </body>
+        `,
+    })
+}
+
+module.exports = { addUser, verifyUser, login }
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express()
 const PORT = 8080;
 const api = require('./api.js')
 const mongoose = require('mongoose')
-const nodemailer = require('nodemailer')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
@@ -37,60 +36,16 @@ app.post('/adduser',
         var username = req.body.username;
         var password = req.body.password;
         var email = req.body.email;
-        api.addUser(username, password, email);
-        // Send a verification email
-        let transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 465, // Secure SMTP port
-            secure: true,
-            auth: {
-                user: process.env.MAIL_USER,
-                pass: process.env.MAIL_PASS,
-            }
-        })
-        transporter.sendMail({
-            from: '"Hot Pink" <team.hotpink.inc@gmail.com>',
-            to: email,
-            subject: "Verify Email Address for hotpink.com", // Subject line
-            text: `Thanks for registering
-            for an account! 
-            Before we get started, 
-            we just need to confirm 
-            that this is you. Click below to verify your email address: 
-            http://localhost:8080/verify?email=${email}&key=abracadabra`,
-            html: `
-            <body>Thanks for registering
-            for an account! 
-            Before we get started, 
-            we just need to confirm 
-            that this is you.<br><br> Click below to verify your email address:<br><br>
-            <a href="http://localhost:8080/verify?email=${email}&key=abracadabra">
-            Verify</a><br><br>${new Date()}</body>`,
-        })
-        res.send(`
-        testing
-            username: ${username},
-            password: ${password},
-            email: ${email}
-        `);
+        // Add user to database
+        api.addUser(username, password, email, res)
 })
 
-// Verifying email through POST (used for grading purposes)
-// ** use Postman to test **
+// Verifying email through POST
 app.post('/verify',
     (req, res) => {
         var email = req.body.email;
         var key = req.body.key;
-        if (key == "abracadabra") {
-            api.verifyUser(email);
-            res.send(`
-                ${email} verified!
-            `);
-        }
-        // Wrong key
-        else {
-            res.status(401).send("Wrong key")
-        }
+        api.verifyUser(email, key, res);
 })
 
 // Optional part, verifies using email link
@@ -98,14 +53,12 @@ app.get('/verify',
     (req, res) => {
         var email = req.query.email
         var key = req.query.key
-        if (key == "abracadabra") {
-            api.verifyUser(email);
-            res.send(`
-                ${email} verified!
-            `);
-        }
-        // Wrong key
-        else {
-            res.status(401).send("Wrong key")
-        }
+        api.verifyUser(email, key, res)
+})
+
+app.post('/login',
+    (req, res) => {
+        var username = req.body.username
+        var password = req.body.password
+        api.login(username, password, res)
 })
